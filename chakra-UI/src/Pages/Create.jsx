2e11d096fcc4db16a857800180973a1c225859b9
Prashanna Lohani <np03cs4s220275@heraldcollege.edu.np>
2023-07-25 14:43:30 +0545

    Chakra-UI Complete

--- chakra-UI/src/Pages/Create.jsx
+++ chakra-UI/src/Pages/Create.jsx
@@ -1,9 +1,42 @@
+import { Box, Button, Checkbox, FormControl, FormHelperText, FormLabel, Input, Textarea } from '@chakra-ui/react'
 import React from 'react'
+import { Form } from 'react-router-dom'
 
-const Create = () => {
+export default function Create ()
+{
   return (
-    <h1 style={{fontSize : "3rem"}}>Create</h1> 
-)
+    <Box maxW='480px'>
+      <Form>
+        <FormControl isRequired mb='40px'>
+          <FormLabel>
+            Task name:
+          </FormLabel>
+          <Input type='text' name='title'  />
+          <FormHelperText>Enter a descriptive task name.</FormHelperText>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>
+            Task Description:
+          </FormLabel>
+          <Textarea
+            placeholder='Enter Detailed description of the task.'
+            name='Description'
+          />
+        </FormControl>
+        <FormControl display='flex' alignItems='center' mt='20px'>
+          <Checkbox
+            name='isPriority'
+            size='lg'
+          />
+          <FormLabel mb='0' ml='10px'>Make this a priority task.</FormLabel>
+        </FormControl>
+        <Button mt='20px' colorScheme='whatsapp'>Submit</Button>
+      </Form>
+    </Box>
+  )
 }
 
-export default Create
+function createAction ()
+{
+
+}
--- chakra-UI/src/Pages/Profile.jsx
+++ chakra-UI/src/Pages/Profile.jsx
@@ -7,7 +7,7 @@ export default function Profile(){
       <Tabs mt='40px' p='20px' colorScheme='blackAlpha' variant='enclosed'>
        <TabList>
          <Tab _selected={{color:'White', bg: 'purple.400'}}>Account Info</Tab>
-         <Tab _selected={{color:'White', bg: 'purple.400'}}>Account Info</Tab>
+         <Tab _selected={{color:'White', bg: 'purple.400'}}>Profile</Tab>
 
 
         </TabList>
